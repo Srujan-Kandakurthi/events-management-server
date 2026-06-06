89e17c400704b9710dc0ed4c07eb7e4d3ae05d46
Srujangupta06 <srujan93811@gmail.com>
2026-06-06 19:48:42 +0530

    Configured Multiple Environments Dev,Production
    Integrated Database based on environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.ENV_TYPE || 'development'}`,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
